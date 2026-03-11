611e21f6799da568fc364d9829ef0fcb7c2f5828
TihonSotnikov <tikhonsotnikov@gmail.com>
2026-03-11 22:06:39 +0700

    о

--- work2/task1.cpp
+++ work2/task1.cpp
@@ -1,47 +1,81 @@
+#include <iostream>
+#include <vector>
 #include <omp.h>
-#include <cstdio>
-#include <cstdlib>
-#include <cmath>
+#include <iomanip>
 
-static void matvec(const double* A, const double* x, double* y, int M) {
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < M; i++) {
-        double sum = 0.0;
-        for (int j = 0; j < M; j++)
-            sum += A[(long)i * M + j] * x[j];
-        y[i] = sum;
+void init_data(double*& vec, double*& matrix, double*& result, int size)
+{
+    vec = new double[size];
+    matrix = new double[(long long)size * size];
+    result = new double[size];
+
+    #pragma omp parallel for
+    for (int i = 0; i < size; i++)
+    {
+        vec[i] = i;
+        result[i] = 0.0;
+        for (int j = 0; j < size; j++)
+            matrix[(long long)i * size + j] = i + j;
     }
 }
 
-int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <M> <threads>\n", argv[0]);
+void free_data(double* vec, double* matrix, double* result)
+{
+    delete[] vec;
+    delete[] matrix;
+    delete[] result;
+}
+
+void matrix_vector_product(const double* vec, const double* matrix, double* result, int size)
+{
+    #pragma omp parallel for
+    for (int i = 0; i < size; i++)
+    {
+        double sum = 0.0;
+        for (int j = 0; j < size; j++)
+            sum += matrix[(long long)i * size + j] * vec[j];
+        result[i] = sum;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <M>\n";
         return 1;
     }
+    
+    int m = std::atoi(argv[1]);
 
-    const int M        = atoi(argv[1]);
-    const int threads  = atoi(argv[2]);
-    omp_set_num_threads(threads);
+    double *vec = nullptr, *matrix = nullptr, *result = nullptr;
+    double T_sequential = 0.0;
 
-    double* A = new double[(long)M * M];
-    double* x = new double[M];
-    double* y = new double[M];
+    std::vector<int> nums_threads = {1, 2, 4, 6, 8, 16, 20, 40};
 
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < M; j++)
-            A[(long)i * M + j] = (i == j) ? 2.0 : 1.0;
-        x[i] = 1.0;
+    std::cout << "Threads  Time(s)  Speedup\n";
+    std::cout << "-------------------------\n";
+
+    std::cout << std::fixed << std::setprecision(5);
+    for (int cur_num_threads : nums_threads)
+    {
+        omp_set_num_threads(cur_num_threads);
+        init_data(vec, matrix, result, m);
+
+        double start_time = omp_get_wtime();
+        matrix_vector_product(vec, matrix, result, m);
+        double end_time = omp_get_wtime();
+        double elapsed_time = end_time - start_time;
+        if (cur_num_threads == 1)
+            T_sequential = elapsed_time;
+        double speedup = T_sequential / elapsed_time;
+
+        std::cout << cur_num_threads << "        " 
+                  << elapsed_time << "  " 
+                  << speedup << '\n';
+
+        free_data(vec, matrix, result);
     }
 
-    double t0 = omp_get_wtime();
-    matvec(A, x, y, M);
-    double elapsed = omp_get_wtime() - t0;
-
-    printf("M=%d  threads=%d  time=%.4f s\n", M, threads, elapsed);
-
-    delete[] A;
-    delete[] x;
-    delete[] y;
     return 0;
 }
