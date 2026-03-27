386a6f4671257f40f2798647ff79562f3acb4ed0
TihonSotnikov <tikhonsotnikov@gmail.com>
2026-03-27 16:03:27 +0700

    task 3 done

--- work2/task3.cpp
+++ work2/task3.cpp
@@ -1,6 +1,14 @@
 #include <omp.h>
 #include <cmath>
 
+#ifdef schedule_dynamic
+    #define s schedule(dynamic)
+#elif schedule_guided
+    #define s schedule(guided)
+#else
+    #define s schedule(static)
+#endif
+
 const double eps = 1e-5;
 
 void find_solution_v1(const double* A, const double* b, double* x, size_t n)
@@ -9,7 +17,7 @@ void find_solution_v1(const double* A, const double* b, double* x, size_t n)
     double* Ax = new double[n];
     
     double b_norm = 0.0;
-    #pragma omp parallel for reduction(+ : b_norm) schedule(static)
+    #pragma omp parallel for reduction(+ : b_norm) s
     for (size_t i = 0; i < n; i++)
         b_norm += b[i] * b[i];
     b_norm = sqrt(b_norm);
@@ -18,7 +26,7 @@ void find_solution_v1(const double* A, const double* b, double* x, size_t n)
 
     for (size_t iter = 0; iter < 10000; iter++)
     {
-        #pragma omp parallel for schedule(static)
+        #pragma omp parallel for s
         for (size_t i = 0; i < n; i++)
         {
             double sum = 0.0;
@@ -28,7 +36,7 @@ void find_solution_v1(const double* A, const double* b, double* x, size_t n)
         }
 
         double norm_sq = 0.0;
-        #pragma omp parallel for reduction(+ : norm_sq) schedule(static)
+        #pragma omp parallel for reduction(+ : norm_sq) s
         for (size_t i = 0; i < n; i++)
         {
             r[i] = Ax[i] - b[i];
@@ -38,7 +46,7 @@ void find_solution_v1(const double* A, const double* b, double* x, size_t n)
         if (sqrt(norm_sq) / b_norm < eps)
             break;
 
-        #pragma omp parallel for schedule(static)
+        #pragma omp parallel for s
         for (size_t i = 0; i < n; i++)
             x[i] -= tau * r[i];
     }
@@ -64,7 +72,7 @@ void find_solution_v2(const double* A, const double* b, double* x, size_t n)
     {
         for (size_t iter = 0; iter < 10000; iter++)
         {
-            #pragma omp for schedule(static)
+            #pragma omp for s
             for (size_t i = 0; i < n; i++)
             {
                 double sum = 0.0;
@@ -76,7 +84,7 @@ void find_solution_v2(const double* A, const double* b, double* x, size_t n)
             #pragma omp single
             norm_sq = 0.0;
 
-            #pragma omp for reduction(+ : norm_sq) schedule(static)
+            #pragma omp for reduction(+ : norm_sq) s
             for (size_t i = 0; i < n; i++)
             {
                 r[i] = Ax[i] - b[i];
@@ -86,7 +94,7 @@ void find_solution_v2(const double* A, const double* b, double* x, size_t n)
             if (sqrt(norm_sq) / b_norm < eps)
                 break;
 
-            #pragma omp for schedule(static)
+            #pragma omp for s
             for (size_t i = 0; i < n; i++)
                 x[i] -= tau * r[i];
         }
